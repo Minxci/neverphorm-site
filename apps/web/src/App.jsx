ae85c4a3f77815ec0b8c1e95c455127941cba501
Cody M <99000120+Minxci@users.noreply.github.com>
2026-05-19 21:55:00 -0500

    im tired dude lol

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -5,6 +5,8 @@ import { Toaster } from 'sonner';
 import ScrollToTop from './components/ScrollToTop.jsx';
 import HomePage from './pages/HomePage.jsx';
 import ContactUsPage from './pages/ContactUsPage.jsx';
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsOfService from "./pages/TermsOfService";
 
 function App() {
   return (
@@ -14,8 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/contact" element={<ContactUsPage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/terms" element={<TermsOfService />} />
       </Routes>
     </Router>
   );
