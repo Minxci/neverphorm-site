0031705a0220eb6edb5a92e3520fb895c40f47fa
Cody M <99000120+Minxci@users.noreply.github.com>
2026-05-26 00:43:33 -0500

    this symbol was lame

--- apps/web/src/pages/HomePage.jsx
+++ apps/web/src/pages/HomePage.jsx
@@ -87,18 +87,6 @@ const HomePage = () => {
               </div>
             </motion.div>
           </div>
-
-          {/* Scroll Indicator */}
-          <motion.div className="absolute bottom-8 left-1/2 -translate-x-1/2" animate={{
-          y: [0, 10, 0]
-        }} transition={{
-          duration: 2,
-          repeat: Infinity
-        }}>
-            <div className="w-6 h-10 border-2 border-primary/50 rounded-full flex items-start justify-center p-2">
-              <div className="w-1.5 h-1.5 bg-primary rounded-full"></div>
-            </div>
-          </motion.div>
         </section>
 
 
@@ -278,7 +266,7 @@ const HomePage = () => {
 
         {/* Contact Section */}
 
-        
+
 
         <div id="contactus">
           <ContactSection />
