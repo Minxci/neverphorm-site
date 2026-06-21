ca91d4b0538cee2df537eeab8f85b45f8c8a8db9
Cody M <99000120+Minxci@users.noreply.github.com>
2026-06-21 14:13:16 -0500

    guess i cant and wont have the gameplay up on the page

--- apps/web/src/pages/Updates.jsx
+++ apps/web/src/pages/Updates.jsx
@@ -7,8 +7,7 @@ export default function Updates() {
       title: "DDodge Update v0.1.9",
       date: "June 19, 2026",
       type: "Devlog",
-      video: "https://www.facebook.com/reel/1015602911454060",
-      text: "Did some major changes to this mobile game, it's been a fun small project. Only have maybe a couple or so more things to polish, add in the global leaderboard via Google Games Service and Apple Game Center, then will be ready to ship! Android build is ready for launch, then iOS, then soon after this it's back to the main game.",
+      text: "Well apparently since I can't upload the gameplay of the game here I supposed I'll just mention which things I did and what is left to add. Did some major changes to this mobile game, it's been a fun small project. Only have maybe a couple or so more things to polish, add in the global leaderboard via Google Games Service and Apple Game Center, then will be ready to ship! Android build is ready for launch, then iOS, then soon after this it's back to the main game.",
     },
     {
       title: "Website Update Version 1.1.0",
