c7a3e090f19ee41d56234293ce817d19f22010a5
Cody M. <codylee_93@live.com>
2026-07-07 23:31:52 -0500

    maybe this minor adjustment works

--- apps/web/src/pages/Mobile/VecPulse.jsx
+++ apps/web/src/pages/Mobile/VecPulse.jsx
@@ -235,6 +235,7 @@ export default function VecPulseOverview() {
               src="/webgl-builds/vecpulse/index.html"
               title="Play VecPulse in your browser"
               className="h-full w-full"
+              allow="fullscreen 'none'"
             />
           </div>
         </div>
