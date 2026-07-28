5c99476d4fb80f459249e03d04af24b2ecf77a07
Cody M <99000120+Minxci@users.noreply.github.com>
2026-07-27 20:41:26 -0500

    really thinning out this site but was a lot that really was unnecessary especially where im realisitcally at

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -4,10 +4,8 @@ import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import ScrollToTop from './components/ScrollToTop.jsx';
 import HomePage from './pages/HomePage.jsx';
-import ContactUsPage from './pages/ContactUsPage.jsx';
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
-import Games from "./pages/Games.jsx";
 import VecPulse from "@/pages/Mobile/VecPulse.jsx";
 import NotFound from "@/pages/NotFound";
 
@@ -18,10 +16,8 @@ function App() {
       <Toaster position="bottom-right" theme="dark" richColors />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/contact" element={<ContactUsPage />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
         <Route path="/terms" element={<TermsOfService />} />
-        <Route path="/games" element={<Games />} />
 
         <Route path="/mobile/vecpulse" element={<VecPulse />} />
         <Route path="*" element={<NotFound />} />
